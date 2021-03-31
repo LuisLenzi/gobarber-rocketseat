54f265b6d027ed8e2c15e633587a7f8a1a8a335e
LuisLenzi <luis.lenzi@nexenergy.com.br>
2021-03-31 18:38:37 -0300

    chore: create AppointmentDTO logic

---
+++ back-end/src/routes/appointments.routes.ts
@@ -0,0 +1,33 @@
+import { Router } from "express";
+import { startOfHour, parseISO } from "date-fns";
+import  AppointmentsRepository  from "../repositories/AppointmentsRepository";
+
+const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.status(200).json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
+
+  if (findAppointmentInSameDate) {
+    return response.status(400).json({ message: "This appointment is already booked, wait for 01 hour please" });
+  }
+
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate
+  })
+
+  return response.json(appointment)
+})
+
+export default appointmentsRouter;
