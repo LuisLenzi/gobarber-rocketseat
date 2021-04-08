366cb92c37580e410fb78dcc265b6a04f42b4406
LuisLenzi <luisgustavolenzi@hotmail.com>
2021-04-08 16:20:23 -0300

    chore: creating connection with docker and typeorm

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,12 +1,14 @@
-import express from 'express'
-import routes from './routes/index'
+import 'reflect-metadata';
+import express from 'express';
+import routes from './routes/index';
+import './database';
 
 const app = express();
 const port = 3333;
 
-app.use(express.json())
+app.use(express.json());
 app.use(routes);
 
 app.listen(port, () => {
-  console.log(`\n [🚀] - Server is running\n [💻] - http://localhost:${port}`)
+  console.log(`\n [🚀] - Server is running\n [💻] - http://localhost:${port}`);
 })
